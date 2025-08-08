1317c36512bbf98fd961bee8b91b9fc2326a32d5
linus <linus@nasa.se>
2025-08-08 23:35:05 +0200

    adds dry run

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ function userIsMod(msg){
     return msg.member.roles.cache.find(r => r.name === "MOD")
 }
 
+async function addLegacyRoleToAll(giveRole) {
+  const guild = client.guilds.cache.get(SERVER_ID);
+    guild.members.cache.forEach(member => {
+      console.log(`Adding legacy role to: ${member.user.username} [${member.user.id}]`)
+      if (giveRole) member.roles.add(LEGACY_ROLE_ID).catch(console.error);
+    });
+}
+
 client.login(process.env.BOT_TOKEN).catch(err => {
   console.error(err);
   process.exit();
@@ -23,14 +31,16 @@ client.on("messageCreate", async msg => {
   if (msg.author.bot) return;
 
   if (userIsMod(msg) && msg.content.split(" ")[0].toLowerCase() == "!addlegacyrole") {
-    const guild = client.guilds.cache.get(SERVER_ID);
-    guild.members.cache.forEach(member => {
-      console.log(`Adding legacy role to: ${member.user.username} [${member.user.id}]`)
-      member.roles.add(LEGACY_ROLE_ID).catch(console.error);
-    });
-
+    await addLegacyRoleToAll(true);
     msg.channel.send("Finnished adding the legacy role to all server members.");
   }
+
+  //dry run
+  if (userIsMod(msg) && msg.content.split(" ")[0].toLowerCase() == "!addlegacyrole-n") {
+    await addLegacyRoleToAll(false);
+    msg.channel.send("Dry run: Finnished adding the legacy role to all server members.");
+  }
+
 })
 
 process.on("exit",  () => {
