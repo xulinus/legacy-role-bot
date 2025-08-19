3334ee0a2bd2316823766c2f692cb28742a924da
linus <linus@nasa.se>
2025-08-19 10:14:53 +0200

    done

--- index.js
+++ index.js
@@ -11,15 +11,122 @@ const SERVER_ID = process.env.SERVER_ID;
 const LEGACY_ROLE_ID = process.env.LEGACY_ROLE_ID;
 
 function userIsMod(msg){
-    return msg.member.roles.cache.find(r => r.name === "MOD")
+  return msg.member.roles.cache.find(r => r.name === "MOD")
 }
 
-async function addLegacyRoleToAll(giveRole) {
-  const guild = client.guilds.cache.get(SERVER_ID);
-    guild.members.cache.forEach(member => {
-      console.log(`Adding legacy role to: ${member.user.username} [${member.user.id}]`)
-      if (giveRole) member.roles.add(LEGACY_ROLE_ID).catch(console.error);
-    });
+function getRoleFromCommand(msg) {
+  return msg.content.split(/\s+/).slice(1).join(" ");
+}
+
+function getRoleId(msg, role) {
+  const cache = msg.guild.roles.cache
+  /*
+   * Try to get the role id by either id or name 
+   */
+  let r = cache.find(r => r.id === role);
+  if (!r) {
+    r = cache.find(r => r.name.toLowerCase() === role.toLowerCase());
+  }
+
+  return r ? r.id : null;
+}
+
+function getRoleName(msg, role) {
+  const cache = msg.guild.roles.cache
+  /*
+   * Try to get the role name by either id or name 
+   */
+  let r = cache.find(r => r.id === role);
+  if (!r) {
+    r = cache.find(r => r.name.toLowerCase() === role.toLowerCase());
+  }
+
+  return r ? r.name : null;
+}
+
+/* 
+ * msg = msg that triggers the bot 
+ * roleId = role to be set 
+ * giveRole = boolean, if false do drive run 
+ */ 
+async function addRoleToAll(msg, roleId, giveRole) {
+  const roleObj = msg.guild.roles.cache.find(r => r.id === roleId)
+  if (roleObj === undefined) return false;
+
+  let success = true;
+
+  for (const member of msg.guild.members.cache.values()) {
+    console.log(`Adding [${roleObj.name}] role to: ${member.user.username} [${member.user.id}]`);
+    if (giveRole) {
+      try {
+        await member.roles.add(roleObj.id);
+      } catch (err) {
+        console.error(err);
+        success = false;
+      }
+    }
+  }
+  
+  return success;
+}
+
+async function addRoleToAllCommand(msg, role, giveRole) {
+  // make sure that role exists, and get the role id 
+  roleId = getRoleId(msg, role);
+
+  if(roleId) {
+    roleName = getRoleName(msg, roleId);
+    
+    if (await addRoleToAll(msg, roleId, giveRole)) {
+      if (giveRole) msg.channel.send(`Finnished adding the role [${roleName}] to all server members.`);
+      if (!giveRole) msg.channel.send(`DRY RUN: Finnished adding the role [${roleName}] to all server members.`);
+      return
+    };
+  }
+  
+  msg.channel.send(`Something went wrong, failed to add role. Make sure that [${role}] is a role.`)
+}
+
+/* 
+ * msg = msg that triggers the bot 
+ * roleId = role to be set 
+ * giveRole = boolean, if false do drive run 
+ */ 
+async function removeRoleFromAll(msg, roleId, removeRole) {
+  const roleObj = msg.guild.roles.cache.find(r => r.id === roleId)
+  if (roleObj === undefined) return false;
+
+  let success = true;
+
+  for (const member of msg.guild.members.cache.values()) {
+    console.log(`Removing [${roleObj.name}] role from: ${member.user.username} [${member.user.id}]`);
+    if (removeRole) {
+      try {
+        await member.roles.remove(roleObj.id);
+      } catch (err) {
+        console.error(err);
+        success = false;
+      }
+    }
+  }
+  
+  return success;
+}
+
+async function removeRoleFromAllCommand(msg, role, removeRole) {
+  // make sure that role exists, and get the role id 
+  roleId = getRoleId(msg, role);
+  
+  if (roleId) {
+    roleName = getRoleName(msg, roleId);
+    if (await removeRoleFromAll(msg, roleId, removeRole)) {
+      if (removeRole) msg.channel.send("Finnished removing role from all server members.");
+      if (!removeRole) msg.channel.send("DRY RUN: Finnished removing role from all server members.");
+      return
+    };
+  }
+
+  msg.channel.send(`Something went wrong, failed to remove role. Make sure that [${role}] is a role.`)
 }
 
 client.login(process.env.BOT_TOKEN).catch(err => {
@@ -30,15 +137,26 @@ client.login(process.env.BOT_TOKEN).catch(err => {
 client.on("messageCreate", async msg => {
   if (msg.author.bot) return;
 
-  if (userIsMod(msg) && msg.content.split(" ")[0].toLowerCase() == "!addlegacyrole") {
-    await addLegacyRoleToAll(true);
-    msg.channel.send("Finnished adding the legacy role to all server members.");
+  if (userIsMod(msg) && msg.content.split(/\s+/)[0].toLowerCase() == "!addrole") {
+    role = getRoleFromCommand(msg); 
+    await addRoleToAllCommand(msg, role, true);
   }
 
   //dry run
-  if (userIsMod(msg) && msg.content.split(" ")[0].toLowerCase() == "!addlegacyrole-n") {
-    await addLegacyRoleToAll(false);
-    msg.channel.send("Dry run: Finnished adding the legacy role to all server members.");
+  if (userIsMod(msg) && msg.content.split(/\s+/)[0].toLowerCase() == "!dryaddrole") {
+    role = getRoleFromCommand(msg); 
+    await addRoleToAllCommand(msg, role, false);
+  }
+
+  if (userIsMod(msg) && msg.content.split(/\s+/)[0].toLowerCase() == "!removerole") {
+    role = getRoleFromCommand(msg); 
+    await removeRoleFromAllCommand(msg, role, true);
+  }
+
+  //dry run
+  if (userIsMod(msg) && msg.content.split(/\s+/)[0].toLowerCase() == "!dryremoverole") {
+    role = getRoleFromCommand(msg); 
+    await removeRoleFromAllCommand(msg, role, false);
   }
 
 })
